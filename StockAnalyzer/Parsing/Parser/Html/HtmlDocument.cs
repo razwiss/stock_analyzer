4af7d61f434a8667f01d7d427a8c87854cc1be01
Pier-Luc Faucher <pl@sigmaoctantis.ca>
2020-03-22 17:14:14 -0400

    Greatly simplified the ClassListCOntains method

--- StockAnalyzer/Parsing/Parser/Html/HtmlDocument.cs
+++ StockAnalyzer/Parsing/Parser/Html/HtmlDocument.cs
@@ -47,28 +47,9 @@ namespace StockAnalyzer.Parsing.Parser.Html
             return nodesConverted;
         }
 
-        private Boolean ClassListContains(String haystack, String needle, StringComparison comparison)
+        private Boolean ClassListContains(string haystack, string needle, StringComparison comparison)
         {
-            if (String.Equals(haystack, needle, comparison)) return true;
-
-            Int32 idx = 0;
-
-            while (idx + needle.Length <= haystack.Length)
-            {
-                idx = haystack.IndexOf(needle, idx, comparison);
-                if (idx == -1) return false;
-
-                Int32 end = idx + needle.Length;
-
-                // Needle must be enclosed in whitespace or be at the start/end of string
-                Boolean validStart = idx == 0 || Char.IsWhiteSpace(haystack[idx - 1]);
-                Boolean validEnd = end == haystack.Length || Char.IsWhiteSpace(haystack[end]);
-                if (validStart && validEnd) return true;
-
-                idx++;
-            }
-
-            return false;
+            return haystack.Split(" ").Where(n => n == needle).Count() > 0;
         }
     }
 }
